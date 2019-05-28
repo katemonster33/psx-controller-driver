2b50a8a89c3c7e031e9256c6cc0d5382e79498c0
Katie McKean <katherine.e.mckean@snapon.com>
2019-05-28 14:18:06 -0400

    made whitespace changes because i felt like it.

--- uinput-client/main.cpp
+++ uinput-client/main.cpp
@@ -1,14 +1,10 @@
 
 #include "linux_uinput.hpp"
 #include "serial_helper.hpp"
-
 #define ARDUINO_DEV "/dev/ttyUSB1"
-
 #define ARDUINO_DEV_1 "/dev/ttyUSB1"
-
 LinuxUinput *player1 = 0;
 LinuxUinput *player2 = 0;
-
 
 int main(int argc, char **argv) {
   SerialHelper serialHelper;
